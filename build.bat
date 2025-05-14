0734f40321da44efd2310bb30485868ced0f29b4
Tosa <eea85670@gmail.com>
2025-05-14 17:46:40 +0400

    1.0

--- build.bat
+++ build.bat
@@ -1,4 +1,2 @@
 cmake . -B build
 cmake --build build
-cd bin/Debug
-Launcher.exe
--- src/main.cpp
+++ src/main.cpp
@@ -5,34 +5,12 @@
 
 std::string title = "Launcher";
 
-// Функция для чтения содержимого файла index.html
-std::string readIndex()
-{
-    std::ifstream in("resources/index.html");
-    // Проверяем, удалось ли открыть файл
-    if (!in) {
-        std::cerr << "Ошибка: не удалось открыть файл resources/index.html" << std::endl;
-        return ""; // Возвращаем пустую строку в случае ошибки
-    }
-
-    // Резервируем память для строки, чтобы избежать многократного выделения
-    std::string content;
-    content.reserve(1024 * 1024); // Предполагаем, что файл не превышает 1 МБ
-
-    std::string line;
-    // Читаем файл построчно и добавляем в строку
-    while (std::getline(in, line)) {
-        content += line; // Конкатенация строк
-    }
-
-    return content; // Возвращаем содержимое файла
-}
-
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nCmdShow)
 {
+    system("resources/node/node.exe resources/server.js");
     webview::webview wview(false, nullptr);
     wview.set_title(title); // Используем переменную title
     wview.set_size(1280, 720, WEBVIEW_HINT_FIXED);
-    wview.set_html(readIndex()); // Устанавливаем HTML содержимое
+    wview.navigate("localhost:3000"); // Подключение к frontend
     wview.run(); // Запускаем веб-просмотр
 }
